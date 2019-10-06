4d68a0b93edd888c3bd263087309d30d7ccc199f
Martynas <martynas.tumas@gmail.com>
2019-10-06 19:23:49 +0200

    Created sequential and parallel methods for seraching prime numbers in range

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace CLI
 {
@@ -6,7 +8,22 @@ namespace CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run(10, 1_000_000);
+            Run(10, 10_000_000);
+            Run(1_000_000, 2_000_000);
+            Run(10_000_000, 20_000_000);
+        }
+
+        static void Run(long lower, long upper)
+        {
+            Console.WriteLine($"Running {lower} to {upper}");
+            var start = DateTime.Now;
+            var primeNumbersParallel = Compulsory_PrimeGen.Class1.GetPrimesParallel(lower, upper);
+            Console.WriteLine($"\tParallel: {DateTime.Now - start}");
+
+            start = DateTime.Now;
+            var primeNumbers = Compulsory_PrimeGen.Class1.GetPrimesSequential(lower, upper).ToList();
+            Console.WriteLine($"\tSequential: {DateTime.Now - start}");
         }
     }
 }
--- Compulsory_PrimeGen/Class1.cs
+++ Compulsory_PrimeGen/Class1.cs
@@ -1,8 +1,45 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Compulsory_PrimeGen
 {
-    public class Class1
+    public static class Class1
     {
+        private static IEnumerable<long> GetPrimes(long first, long last)
+        {
+            for (long noToCheck = first; noToCheck <= last; noToCheck++)
+            {
+                long i, flag = 0;
+
+                var m = noToCheck / 2;
+                for (i = 2; i <= m; i++)
+                {
+                    if (noToCheck % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                {
+                    yield return noToCheck;
+                }
+            }
+        }
+
+        public static List<long> GetPrimesSequential(long first, long last) => GetPrimes(first,last).ToList();
+
+        public static List<long> GetPrimesParallel(long first, long last)
+        {
+            var result = new ConcurrentBag<List<long>>();
+            Parallel.ForEach(Partitioner.Create(first, last), range =>
+            {
+                result.Add(GetPrimes(range.Item1, range.Item2).ToList());
+            });
+            return result.OrderBy(l => l.FirstOrDefault()).SelectMany(l=>l).ToList();
+        }
     }
 }
