d8427d0fe7b027fd5b642deb96e4461ad6a1c802
J2ghz <j2.00ghz@gmail.com>
2019-10-09 12:17:14 +0200

    GUI works

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Compulsory_PrimeGen;
 
 namespace GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void ParallelButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!long.TryParse(lower.Text, out var l))
+                MessageBox.Show($"{lower.Text} is not a number", "Parsing error", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+            if (!long.TryParse(upper.Text, out var u))
+                MessageBox.Show($"{upper.Text} is not a number", "Parsing error", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+
+            List<long> primes = null;
+            await Task.Run(() => { primes = Class1.GetPrimesParallel(l, u); }).ConfigureAwait(false);
+
+
+            Dispatcher.Invoke(() =>
+            {
+                foreach (var prime in primes) ListBox.Items.Add(prime);
+            });
+        }
+
+        private async void SequentialButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!long.TryParse(lower.Text, out var l))
+                MessageBox.Show($"{lower.Text} is not a number", "Parsing error", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+            if (!long.TryParse(upper.Text, out var u))
+                MessageBox.Show($"{upper.Text} is not a number", "Parsing error", MessageBoxButton.OK,
+                    MessageBoxImage.Exclamation);
+
+            List<long> primes = null;
+            await Task.Run(() => { primes = Class1.GetPrimesSequential(l, u); }).ConfigureAwait(false);
+
+
+            Dispatcher.Invoke(() =>
+            {
+                foreach (var prime in primes) ListBox.Items.Add(prime);
+            });
         }
     }
 }
